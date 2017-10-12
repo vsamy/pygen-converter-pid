6e2f53ac2ab39578738cb3cabaf3305ae0ca1b3c
Vincent Samy <vincent.samy@lirmm.fr>
2017-10-12 17:14:49 +0200

    Correct bug on __init__.py lib

--- src/pyTest_lib/__init__.py
+++ src/pyTest_lib/__init__.py
@@ -1,86 +1,86 @@
 #### Matrix and vectors
 # int
-from pyTestFunctions import RandomDynamicSizeMatrixInt
-from pyTestFunctions import RandomFixedSizeMatrixInt
-from pyTestFunctions import RandomHalfDynamicSizeMatrixInt
-from pyTestFunctions import RandomDynamicSizeVectorInt
-from pyTestFunctions import RandomDynamicSizeRowVectorInt
-from pyTestFunctions import RandomFixedSizeVectorInt
-from pyTestFunctions import RandomFixedSizeRowVectorInt
+from .pyTestFunctions import RandomDynamicSizeMatrixInt
+from .pyTestFunctions import RandomFixedSizeMatrixInt
+from .pyTestFunctions import RandomHalfDynamicSizeMatrixInt
+from .pyTestFunctions import RandomDynamicSizeVectorInt
+from .pyTestFunctions import RandomDynamicSizeRowVectorInt
+from .pyTestFunctions import RandomFixedSizeVectorInt
+from .pyTestFunctions import RandomFixedSizeRowVectorInt
 # float32
-from pyTestFunctions import RandomDynamicSizeMatrixFloat
-from pyTestFunctions import RandomFixedSizeMatrixFloat
-from pyTestFunctions import RandomHalfDynamicSizeMatrixFloat
-from pyTestFunctions import RandomDynamicSizeVectorFloat
-from pyTestFunctions import RandomDynamicSizeRowVectorFloat
-from pyTestFunctions import RandomFixedSizeVectorFloat
-from pyTestFunctions import RandomFixedSizeRowVectorFloat
+from .pyTestFunctions import RandomDynamicSizeMatrixFloat
+from .pyTestFunctions import RandomFixedSizeMatrixFloat
+from .pyTestFunctions import RandomHalfDynamicSizeMatrixFloat
+from .pyTestFunctions import RandomDynamicSizeVectorFloat
+from .pyTestFunctions import RandomDynamicSizeRowVectorFloat
+from .pyTestFunctions import RandomFixedSizeVectorFloat
+from .pyTestFunctions import RandomFixedSizeRowVectorFloat
 # float
-from pyTestFunctions import RandomDynamicSizeMatrixDouble
-from pyTestFunctions import RandomFixedSizeMatrixDouble
-from pyTestFunctions import RandomHalfDynamicSizeMatrixDouble
-from pyTestFunctions import RandomDynamicSizeVectorDouble
-from pyTestFunctions import RandomDynamicSizeRowVectorDouble
-from pyTestFunctions import RandomFixedSizeVectorDouble
-from pyTestFunctions import RandomFixedSizeRowVectorDouble
+from .pyTestFunctions import RandomDynamicSizeMatrixDouble
+from .pyTestFunctions import RandomFixedSizeMatrixDouble
+from .pyTestFunctions import RandomHalfDynamicSizeMatrixDouble
+from .pyTestFunctions import RandomDynamicSizeVectorDouble
+from .pyTestFunctions import RandomDynamicSizeRowVectorDouble
+from .pyTestFunctions import RandomFixedSizeVectorDouble
+from .pyTestFunctions import RandomFixedSizeRowVectorDouble
 # complex float32
-from pyTestFunctions import RandomDynamicSizeMatrixCFloat
-from pyTestFunctions import RandomFixedSizeMatrixCFloat
-from pyTestFunctions import RandomHalfDynamicSizeMatrixCFloat
-from pyTestFunctions import RandomDynamicSizeVectorCFloat
-from pyTestFunctions import RandomDynamicSizeRowVectorCFloat
-from pyTestFunctions import RandomFixedSizeVectorCFloat
-from pyTestFunctions import RandomFixedSizeRowVectorCFloat
+from .pyTestFunctions import RandomDynamicSizeMatrixCFloat
+from .pyTestFunctions import RandomFixedSizeMatrixCFloat
+from .pyTestFunctions import RandomHalfDynamicSizeMatrixCFloat
+from .pyTestFunctions import RandomDynamicSizeVectorCFloat
+from .pyTestFunctions import RandomDynamicSizeRowVectorCFloat
+from .pyTestFunctions import RandomFixedSizeVectorCFloat
+from .pyTestFunctions import RandomFixedSizeRowVectorCFloat
 # complex float
-from pyTestFunctions import RandomDynamicSizeMatrixCDouble
-from pyTestFunctions import RandomFixedSizeMatrixCDouble
-from pyTestFunctions import RandomHalfDynamicSizeMatrixCDouble
-from pyTestFunctions import RandomDynamicSizeVectorCDouble
-from pyTestFunctions import RandomDynamicSizeRowVectorCDouble
-from pyTestFunctions import RandomFixedSizeVectorCDouble
-from pyTestFunctions import RandomFixedSizeRowVectorCDouble
+from .pyTestFunctions import RandomDynamicSizeMatrixCDouble
+from .pyTestFunctions import RandomFixedSizeMatrixCDouble
+from .pyTestFunctions import RandomHalfDynamicSizeMatrixCDouble
+from .pyTestFunctions import RandomDynamicSizeVectorCDouble
+from .pyTestFunctions import RandomDynamicSizeRowVectorCDouble
+from .pyTestFunctions import RandomFixedSizeVectorCDouble
+from .pyTestFunctions import RandomFixedSizeRowVectorCDouble
 
 #### Array
 # int
-from pyTestFunctions import RandomDynamicSizeArrayInt
-from pyTestFunctions import RandomFixedSizeArrayInt
-from pyTestFunctions import RandomHalfDynamicSizeArrayInt
-from pyTestFunctions import RandomDynamicSizeColumnArrayInt
-from pyTestFunctions import RandomDynamicSizeRowArrayInt
-from pyTestFunctions import RandomFixedSizeColumnArrayInt
-from pyTestFunctions import RandomFixedSizeRowArrayInt
+from .pyTestFunctions import RandomDynamicSizeArrayInt
+from .pyTestFunctions import RandomFixedSizeArrayInt
+from .pyTestFunctions import RandomHalfDynamicSizeArrayInt
+from .pyTestFunctions import RandomDynamicSizeColumnArrayInt
+from .pyTestFunctions import RandomDynamicSizeRowArrayInt
+from .pyTestFunctions import RandomFixedSizeColumnArrayInt
+from .pyTestFunctions import RandomFixedSizeRowArrayInt
 # float32
-from pyTestFunctions import RandomDynamicSizeArrayFloat
-from pyTestFunctions import RandomFixedSizeArrayFloat
-from pyTestFunctions import RandomHalfDynamicSizeArrayFloat
-from pyTestFunctions import RandomDynamicSizeColumnArrayFloat
-from pyTestFunctions import RandomDynamicSizeRowArrayFloat
-from pyTestFunctions import RandomFixedSizeColumnArrayFloat
-from pyTestFunctions import RandomFixedSizeRowArrayFloat
+from .pyTestFunctions import RandomDynamicSizeArrayFloat
+from .pyTestFunctions import RandomFixedSizeArrayFloat
+from .pyTestFunctions import RandomHalfDynamicSizeArrayFloat
+from .pyTestFunctions import RandomDynamicSizeColumnArrayFloat
+from .pyTestFunctions import RandomDynamicSizeRowArrayFloat
+from .pyTestFunctions import RandomFixedSizeColumnArrayFloat
+from .pyTestFunctions import RandomFixedSizeRowArrayFloat
 # float
-from pyTestFunctions import RandomDynamicSizeArrayDouble
-from pyTestFunctions import RandomFixedSizeArrayDouble
-from pyTestFunctions import RandomHalfDynamicSizeArrayDouble
-from pyTestFunctions import RandomDynamicSizeColumnArrayDouble
-from pyTestFunctions import RandomDynamicSizeRowArrayDouble
-from pyTestFunctions import RandomFixedSizeColumnArrayDouble
-from pyTestFunctions import RandomFixedSizeRowArrayDouble
+from .pyTestFunctions import RandomDynamicSizeArrayDouble
+from .pyTestFunctions import RandomFixedSizeArrayDouble
+from .pyTestFunctions import RandomHalfDynamicSizeArrayDouble
+from .pyTestFunctions import RandomDynamicSizeColumnArrayDouble
+from .pyTestFunctions import RandomDynamicSizeRowArrayDouble
+from .pyTestFunctions import RandomFixedSizeColumnArrayDouble
+from .pyTestFunctions import RandomFixedSizeRowArrayDouble
 # complex float32
-from pyTestFunctions import RandomDynamicSizeArrayCFloat
-from pyTestFunctions import RandomFixedSizeArrayCFloat
-from pyTestFunctions import RandomHalfDynamicSizeArrayCFloat
-from pyTestFunctions import RandomDynamicSizeColumnArrayCFloat
-from pyTestFunctions import RandomDynamicSizeRowArrayCFloat
-from pyTestFunctions import RandomFixedSizeColumnArrayCFloat
-from pyTestFunctions import RandomFixedSizeRowArrayCFloat
+from .pyTestFunctions import RandomDynamicSizeArrayCFloat
+from .pyTestFunctions import RandomFixedSizeArrayCFloat
+from .pyTestFunctions import RandomHalfDynamicSizeArrayCFloat
+from .pyTestFunctions import RandomDynamicSizeColumnArrayCFloat
+from .pyTestFunctions import RandomDynamicSizeRowArrayCFloat
+from .pyTestFunctions import RandomFixedSizeColumnArrayCFloat
+from .pyTestFunctions import RandomFixedSizeRowArrayCFloat
 # complex float
-from pyTestFunctions import RandomDynamicSizeArrayCDouble
-from pyTestFunctions import RandomFixedSizeArrayCDouble
-from pyTestFunctions import RandomHalfDynamicSizeArrayCDouble
-from pyTestFunctions import RandomDynamicSizeColumnArrayCDouble
-from pyTestFunctions import RandomDynamicSizeRowArrayCDouble
-from pyTestFunctions import RandomFixedSizeColumnArrayCDouble
-from pyTestFunctions import RandomFixedSizeRowArrayCDouble
+from .pyTestFunctions import RandomDynamicSizeArrayCDouble
+from .pyTestFunctions import RandomFixedSizeArrayCDouble
+from .pyTestFunctions import RandomHalfDynamicSizeArrayCDouble
+from .pyTestFunctions import RandomDynamicSizeColumnArrayCDouble
+from .pyTestFunctions import RandomDynamicSizeRowArrayCDouble
+from .pyTestFunctions import RandomFixedSizeColumnArrayCDouble
+from .pyTestFunctions import RandomFixedSizeRowArrayCDouble
 
 __all__ = ["RandomDynamicSizeMatrixInt",
            "RandomFixedSizeMatrixInt",
